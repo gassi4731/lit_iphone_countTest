7f2e2b04cd453e7b5c92828546878b2a44fee4f6
Yo Higashida <penguin4731@gmail.com>
2021-02-08 13:57:10 +0900

    完成🎉

--- CountTest/ViewController.swift
+++ CountTest/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func plus() {
+        number = number + 1
+        label.text = String(number)
+    }
+    
+    @IBAction func minus() {
+        number = number - 1
+        label.text = String(number)
+    }
+    
+    @IBAction func clear() {
+        number = 0
+        label.text = String(number)
+    }
 }
 
